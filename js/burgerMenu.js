a7054a3b63407aac421ee117e44886bb6804da10
pengzhangg <pengzhang720@gmail.com>
2024-07-14 20:13:55 +1200

    completed initial website layout design

---
+++ js/burgerMenu.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navList = document.querySelector('.nav-list');
+
+    navToggle.addEventListener('click', function () {
+        navList.classList.toggle('active');
+    });
+});
+
